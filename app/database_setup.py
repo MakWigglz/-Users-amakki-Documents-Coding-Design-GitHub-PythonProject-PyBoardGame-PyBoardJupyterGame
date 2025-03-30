46697066cee88b5ea4c3dda6f01c1766128bac13
MakWigglz <ahmadmakki74@gmail.com>
2025-03-30 11:42:28 +0300

    migrate to db table

--- app/database_setup.py
+++ app/database_setup.py
@@ -17,7 +17,10 @@ def setup_database(db_path, json_path):
         for item in data:
             cursor.executemany('''
     INSERT OR REPLACE INTO subjects (title, content)
-    VALUES (?, ?);
+    VALUES ('Geology', 'Geology is the study of the Earth's physical structure, substances, and processes. It investigates how tectonic plates shape mountains and valleys. Volcanology explores magma, eruptions, and lava formations. Seismology studies earthquakes and seismic waves. Mineralogy examines the chemical composition of Earth's minerals. Paleontology, a subfield, analyzes fossils to uncover past life forms. Geomorphology explains how landscapes evolve over time. Hydrogeology looks at groundwater flow and its impact. Stratigraphy examines layered rock to interpret Earth's historical changes. Geologists study resources like oil, gas, and metals. Environmental geology assesses how human activities affect our planet, promoting sustainability and disaster mitigation.'),
+    ('Chemistry', 'Chemistry is the study of the properties and behavior of matter. It includes valent and covalent bonds of the electron shells or chemical compositions that make up complex biological and physical structures. At the fundamental level we can talk about quantum physics as the basic composition of matter.'),
+    ('Mathematics', 'Mathematics is the abstract science of numbers, quantity, and space. Arithmetic forms the basis, focusing on numbers and operations. Algebra introduces variables and equations. Geometry explores shapes, sizes, and properties of space. Trigonometry studies angles and their relationships. Calculus deals with change and motion, using derivatives and integrals. Probability and statistics analyze uncertainty and data patterns. Discrete mathematics examines finite systems. Topology investigates spatial properties that remain unchanged through deformation. Applied mathematics solves real-world problems in physics, engineering, and economics. Mathematics is vital for technological advancements, fostering logic and innovation in numerous domains.')
+    ;
 ''', [(entry['title'], entry['content']) for entry in data])
             
     conn.commit()
